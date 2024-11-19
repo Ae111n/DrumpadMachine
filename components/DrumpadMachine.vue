f507081c2323f8fb2c0bd5a3c54ea3c7ee829a17
Ae111n <bouguedraaymen@gmail.com>
2024-11-19 14:07:50 +0100

    Drumpad Machine v1.0

--- components/DrumpadMachine.vue
+++ components/DrumpadMachine.vue
@@ -1,8 +1,5 @@
 <template>
     <div id="drum-machine">
-
-
-
         <div id="pads">
             <button @click="playSound('heater1')" ref="Q" class="drum-pad" id="Q">Q</button>
             <button @click="playSound('heater2')" ref="W" class="drum-pad" id="W">W</button>
@@ -14,16 +11,15 @@
             <button @click="playSound('kick')" ref="X" class="drum-pad" id="X">X</button>
             <button @click="playSound('closedHH')" ref="C" class="drum-pad" id="C">C</button>
         </div>
-
         <div id="display">
-<div id="sound-name">
-    {{ CurrentSound }}
-</div>
-<div id="slider">
-<input v-model="currentVolume" type="range" id="volumeFader" name="volume" min="0" max="1" value="1" step="0.1" oninput="setUserVolume(this.value)">
-</div>
+            <div id="sound-name">
+                {{ currentSound }}
+            </div>
+            <div id="slider">
+                <input v-model="currentVolume" type="range" id="volume-fader" class="volume-fader" name="volume" min="0"
+                    max="1" value="1" step="0.1" oninput="setUserVolume(this.value)">
+            </div>
         </div>
-
     </div>
 </template>
 
@@ -42,10 +38,8 @@ export default {
                 kick: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3",
                 closedHH: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"
             },
-
-            CurrentSound:'...',
-            currentVolume:'1'
-
+            currentSound: '...',
+            currentVolume: '1'
         }
     },
     mounted() {
@@ -56,52 +50,46 @@ export default {
             let audio = new Audio(this.sounds[soundName])
             audio.volume = this.currentVolume
             audio.load()
-            audio.play()
-            ,
-            this.CurrentSound = soundName
+            audio.play(),
+                this.currentSound = soundName
         },
         setUserVolume(n) {
 
         },
-
-
-        clickk(ref) {
+        clickButton(ref) {
             ref.focus()
             ref.click()
-
         },
-
         onKeyDown(e) {
 
             if (e.key === 'q' || e.key === 'Q') {
-                this.clickk(Q)
+                this.clickButton(Q)
             }
 
             else if (e.key === 'w' || e.key === 'W') {
-                this.clickk(W)
+                this.clickButton(W)
             }
             else if (e.key === 'e' || e.key === 'E') {
-                this.clickk(E)
+                this.clickButton(E)
             }
             else if (e.key === 'a' || e.key === 'A') {
-                this.clickk(A)
+                this.clickButton(A)
             }
             else if (e.key === 's' || e.key === 'S') {
-                this.clickk(S)
+                this.clickButton(S)
             }
             else if (e.key === 'd' || e.key === 'D') {
-                this.clickk(D)
+                this.clickButton(D)
             }
             else if (e.key === 'z' || e.key === 'Z') {
-                this.clickk(Z)
+                this.clickButton(Z)
             }
             else if (e.key === 'x' || e.key === 'X') {
-                this.clickk(X)
+                this.clickButton(X)
             }
             else if (e.key === 'c' || e.key === 'C') {
-                this.clickk(C)
+                this.clickButton(C)
             }
-
         }
     }
     ,
@@ -109,104 +97,136 @@ export default {
 </script>
 
 <style>
-#sound-name {
-    font-size: 28px;
-    font-family:  sans-serif;
-    margin: auto;
-    border: 1px solid grey ;
-    width: 160px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
+* {
+    background-color: dimgray;
+}
 
-}
-#display {
-    border: 1px solid green;
-    width: 450px;
-    height: 350px;
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    
-}
-#slider {
-border: 1px solid grey ;
-height: 20px;
-margin: auto;
-}
 #drum-machine {
-    border: 1px solid black;
-    width: 900px;
-    height: 400px;
+    border: 5px solid black;
+    border-radius: 5px;
+    width: 800px;
+    height: 360px;
     display: flex;
     flex-direction: row;
-    margin: auto;
+    margin: 150px auto;
+    background-color: rgb(238, 129, 19);
 }
 
 #pads {
-    border: 1px solid red;
+    background-color: rgb(238, 129, 19);
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-    width: 400px;
-    height: 400px;
+    width: 369px;
+    height: 290px;
     font: bold;
+    margin: auto;
 }
 
 .drum-pad {
     width: 100px;
     flex: 0 0 113px;
-    margin: 10px;
 }
 
-
-
 .drum-pad {
-    align-items: center;
-    appearance: none;
-    background-color: #8787a2;
+    appearance: button;
+    background-color: #000;
+    background-image: none;
+    border: 1px solid #000;
     border-radius: 4px;
-    border-width: 0;
-    box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+    box-shadow: #fff 4px 4px 0 0, #000 4px 4px 0 1px;
     box-sizing: border-box;
-    color: #36395A;
+    color: #fff;
     cursor: pointer;
-    display: inline-flex;
-    font-family: "JetBrains Mono", monospace;
-    height: 100px;
-    justify-content: center;
-    line-height: 1;
-    list-style: none;
-    overflow: hidden;
-    padding-left: 16px;
-    padding-right: 16px;
-    position: relative;
-    text-align: left;
-    text-decoration: none;
-    transition: box-shadow .15s, transform .15s;
+    display: inline-block;
+    font-family: ITCAvantGardeStd-Bk, Arial, sans-serif;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 20px;
+    margin: 0 10px 11px 0;
+    overflow: visible;
+    padding: 12px 40px;
+    text-align: center;
+    text-transform: none;
+    touch-action: manipulation;
     user-select: none;
     -webkit-user-select: none;
-    touch-action: manipulation;
+    vertical-align: middle;
     white-space: nowrap;
-    will-change: box-shadow, transform;
-    font-size: 18px;
 }
 
 .drum-pad:focus {
-    transform: translateY(2.9px);
-    outline: none;
-    box-shadow: #D6D6E7 0 0 0 2px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+    text-decoration: none;
 }
 
 .drum-pad:hover {
-    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
-    transform: translateY(-2px);
+    text-decoration: none;
 }
 
 .drum-pad:active {
-    box-shadow: #D6D6E7 0 3px 7px inset;
-    transform: translateY(2px);
+    box-shadow: rgba(0, 0, 0, .125) 0 3px 5px inset;
+    outline: 0;
+}
+
+.drum-pad:not([disabled]):active {
+    box-shadow: #fff 2px 2px 0 0, #000 2px 2px 0 1px;
+    transform: translate(2px, 2px);
+}
+
+@media (min-width: 768px) {
+    .drum-pad {
+        padding: 12px 50px;
+    }
+}
+
+#display {
+    width: 350px;
+    height: 300px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    background-color: rgb(238, 129, 19);
+}
+
+#sound-name {
+    font-size: 28px;
+    font-family: sans-serif;
+    background-color: #2b2b2b;
+    color: rgb(252, 249, 249);
+    box-shadow: rgba(0, 0, 0, 0.56) 2px 6px 22px 7px;
+    margin: auto;
+    border-radius: 5px;
+    width: 180px;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+#slider {
+    margin: auto;
+    background-color: rgb(238, 129, 19);
+}
+
+.volume-fader {
+    -webkit-appearance: none;
+    width: 50%;
+    height: 10px;
+    width: 200px;
+    background: #000;
+    outline: none;
+    border: 1px solid rgb(211, 208, 208);
+    border-radius: 1px;
+}
+
+.volume-fader::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 12px;
+    height: 40px;
+    background: #000;
+    cursor: pointer;
+    border: 1px solid rgb(211, 208, 208);
+    border-radius: 1px;
 }
 </style>
